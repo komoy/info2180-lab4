838c0cb2aa56f4aeef1ab10190ff068d0464b47a
Komoy Haye <komoyhaye16@gmail.com>
2017-10-11 13:45:01 -0500

    exercise1

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
-window.onload=load
+/*window.onload=load
 
 function load()
 {
@@ -11,4 +11,17 @@ function load()
 	}
 
 
+}*/
+window.onload=load
+
+function load()
+{
+	let boundary= document.getElementById("boundary1");
+
+	boundary.onmouseover=function()
+	{
+		//this.className=this.className+"youlose";
+		this.className=this.className+" youlose";
+
+	}
 }
