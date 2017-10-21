b10d3ecb14f323cfdc3d890a6206894ae81a6c1b
Komoy Haye <komoyhaye16@gmail.com>
2017-10-20 19:01:02 -0500

    Update maze.js
    creating a gh-page

--- maze.js
+++ maze.js
@@ -1,14 +1,3 @@
-/*window.onload=load
-function load()
-{
-	let boundary=document.getElementsByClassName("boundary");
-	for (var i =0; i< boundary.length;  i++) {
-		boundary[i].onmouseover= function(){
-			this.className=this.className+" youlose";
-		}
-	}
-}*/
-
 //Exercise 1
 window.onload=load
 
@@ -34,45 +23,7 @@ function load()
 				allBoundary[i].className+=" youlose";
 			}
 		}
-		//
-
-/*window.onload=load
-function load()
-{
-	let boundary=document.getElementsByClassName("boundary");
-	for (var i =0; i< boundary.length;  i++) {
-		boundary[i].onmouseover= function(){
-			this.className=this.className+" youlose";
-		}
-	}
-}*/
-
-//Exercise 1
-window.onload=load
-
-function load()
-{
-	let boundary= document.getElementById("boundary1");
-
-	boundary.onmouseover=function()
-	{
-		
-		this.className=this.className+" youlose";
-
-	}
-	//Exercise2
-	let allBoundary=document.querySelectorAll(".boundary")
-
-	for (var i =0; i< allBoundary.length;  i++) {
-		allBoundary[i].onmouseover=redOnHover;
-	}
-	
-	 function redOnHover(){
-			for (var i=0; i < allBoundary.length; i++) {
-				allBoundary[i].className+=" youlose";
-			}
-		}
-		var loser = false;  // whether the user has hit a wall
+		var loser = false; // when the user hits a wall
 
 
     $("start").onclick = startClick;
